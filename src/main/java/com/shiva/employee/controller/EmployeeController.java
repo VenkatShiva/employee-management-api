1bfa1a964c37bdc8fd5d68fb9ddb72b6219d8819
VenkatShiva <avenkatashiva@gmail.com>
2026-08-26 21:50:08 +0530

    springboot integrated

--- src/main/java/com/shiva/employee/controller/EmployeeController.java
+++ src/main/java/com/shiva/employee/controller/EmployeeController.java
@@ -1,9 +1,22 @@
-
 package com.shiva.employee.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.shiva.employee.service.EmployeeService;
 
 @RestController
 public class EmployeeController {
-    
+
+    private final EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Employee Management API is running";
+    }
+
 }
